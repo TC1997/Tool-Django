5e964573bf4dd45b5cc56c3523fa11f04e6bb407
Chenyuan <cyuanzhang19@gmail.com>
2019-12-07 10:37:10 -0500

    add view_map

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from .forms import SquirrelForm, SquirrelForm1
+import numpy as np
+
+from .models import Squirrel
+
+
+def map(request):
+    sightings0 = Squirrel.objects.all()
+    # sightings = sightings0[:10]
+    sightings_loc_ = []
+    sightings = []
+    for i in range(120):
+        sightings_loc = [sightings0[i].Latitude, sightings0[i].Longitude]
+        if sightings_loc not in sightings_loc_:
+            sightings_loc_.append(sightings_loc)
+            sightings.append(sightings0[i])
+        if len(sightings) == 100:
+            break
+    context = {'sightings': sightings}
+    return render(request, 'myApp/map.html', context)
