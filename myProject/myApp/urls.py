99332053335e9ef069ad8bd1500066d19b142170
TC1997 <tc3026@columbia.edu>
2019-12-07 12:56:46 -0500

    added some urls

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('map/', views.map, name='map'),
+    path('sightings/', views.list, name='list'),
+    path('sightings/<int:unique_squirrel_id>/', views.update, name='update'),
+    path('sightings/add/', views.create, name='create'),
 ]
