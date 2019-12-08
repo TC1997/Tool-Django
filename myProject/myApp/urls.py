af1a6e08b09d6c2d840a813ec9f6b0fd872bbd50
TC1997 <tc3026@columbia.edu>
2019-12-07 20:11:34 -0500

    added stats url

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('sightings/<int:unique_squirrel_id>/', views.update, name='update'),
     path('sightings/add/', views.create, name='create'),
     path('sightings/<int:unique_squirrel_id>/delete', views.delete, name='delete'),
+    path('sightings/stats/', views.stats, name='stats')
 ]
