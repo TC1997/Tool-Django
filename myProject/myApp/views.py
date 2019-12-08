059f5c28f91ac5aee5031274ad690e7d05ad672a
TC1997 <tc3026@columbia.edu>
2019-12-07 20:14:45 -0500

    added stats into views

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -55,3 +55,35 @@ def delete(request, unique_squirrel_id):
     squirrel.delete()
     return redirect('/sightings')
 
+def stats(request):
+    squirrel_list = Squirrel.objects.all()
+    latitude_ = []
+    longitude_ = []
+    location_ = []
+    other_activities_ = []
+    primary_fur_color_ = []
+    for squirrel in squirrel_list:
+        latitude_.append(squirrel.Latitude)
+        longitude_.append(squirrel.Longitude)
+        location_.append(squirrel.Location)
+        other_activities_.append(squirrel.Other_Activities)
+        primary_fur_color_.append(squirrel.Primary_Fur_Color)
+    MLati = '1'#np.mean(np.array(latitude_).astype(np.float))
+    MLongi ='2' #np.mean(np.array(longitude_).astype(np.float))
+
+
+
+    def count_(lst):
+        return len([x for x in lst if x])
+
+    NLoca = count_(location_)
+    NOA = count_(other_activities_)
+    NPFC = count_(primary_fur_color_)
+    dict_ = {
+        'Mean_of_Latitude': MLati,
+        'Mean_of_Longitude': MLongi,
+        'Count_of_Location': NLoca,
+        'Count_of_Other_Activities': NOA,
+        'Count_of_Primary_Fur_Color': NPFC
+    }
+    return render(request, 'myApp/stats.html', dict_)
