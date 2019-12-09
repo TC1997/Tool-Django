e4e64c914b11875bc7abede01c53988e6210c781
Chenyuan <cyuanzhang19@gmail.com>
2019-12-08 20:01:25 -0500

    change url(stats) sequence

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('map/', views.map, name='map'),
     path('sightings/', views.list, name='list'),
     path('sightings/add/', views.create, name='create'),
+    path('sightings/stats/', views.stats, name='stats'),
     path('sightings/<str:unique_squirrel_id>/', views.update, name='update'),
     path('sightings/<str:unique_squirrel_id>/delete', views.delete, name='delete'),
-    path('sightings/stats/', views.stats, name='stats')
 ]
