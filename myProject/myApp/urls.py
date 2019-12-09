0a86f2e5abb7ebaecdb9b43144a15ccb6914ce7c
Chenyuan <cyuanzhang19@gmail.com>
2019-12-08 19:44:59 -0500

    convert url variable form int to string

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('map/', views.map, name='map'),
     path('sightings/', views.list, name='list'),
-    path('sightings/<int:unique_squirrel_id>/', views.update, name='update'),
     path('sightings/add/', views.create, name='create'),
-    path('sightings/<int:unique_squirrel_id>/delete', views.delete, name='delete'),
+    path('sightings/<str:unique_squirrel_id>/', views.update, name='update'),
+    path('sightings/<str:unique_squirrel_id>/delete', views.delete, name='delete'),
     path('sightings/stats/', views.stats, name='stats')
 ]
