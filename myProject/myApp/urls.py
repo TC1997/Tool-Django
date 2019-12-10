476389c7d31183307dfc8d8df88c5383b53faad3
cyuanzhang <55394566+cyuanzhang@users.noreply.github.com>
2019-12-09 20:26:18 -0500

    add comments for urls.py

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('map/', views.map, name='map'),
     path('sightings/', views.list, name='list'),
-    path('sightings/add/', views.create, name='create'),
+    path('sightings/add/', views.create, name='create'),  # note the sequence here
     path('sightings/stats/', views.stats, name='stats'),
     path('sightings/<str:unique_squirrel_id>/', views.update, name='update'),
     path('sightings/<str:unique_squirrel_id>/delete', views.delete, name='delete'),
